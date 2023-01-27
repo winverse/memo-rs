438e7bad6f3ff023d8fc566f8be788872e190a36
sangHyun <public.winverse@gmail.com>
2023-01-27 16:06:04 +0900

    Completed

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -5,13 +5,18 @@ mod http;
 mod server;
 mod website_handler;
 
+use std::env;
+
 use http::{Method, Request};
 use server::Server;
 use website_handler::WebSiteHandler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    // PUBLIC_PATH=$(pwd)/public cargo run : 이런식으로 실행이 가능함
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path: {}", public_path);
     let address = String::from("127.0.0.1:8080");
-
     let server = Server::new(address);
-    server.run(WebSiteHandler);
+    server.run(WebSiteHandler::new(public_path));
 }
--- http-server/src/website_handler.rs
+++ http-server/src/website_handler.rs
@@ -1,15 +1,46 @@
+use std::fs;
+
 use crate::http::{Method, Request, Response, StatusCode};
 
 use super::server::Handler;
 
-pub struct WebSiteHandler;
+pub struct WebSiteHandler {
+    public_path: String,
+}
+impl WebSiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self {
+            public_path: public_path,
+        }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        // ../../../server/index.html -> /server/index.html로 변경 해줌
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attemped: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
+}
+
 impl Handler for WebSiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
