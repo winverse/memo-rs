71281222e3207d16e2f51106cb643eea67a78b84
sangHyun <public.winverse@gmail.com>
2023-03-06 19:03:08 +0900

    Served JavaScript file when html loaded

--- htop/src/main.rs
+++ htop/src/main.rs
@@ -1,4 +1,10 @@
-use axum::{extract::State, response::IntoResponse, routing::get, Json, Router, Server};
+use axum::{
+    extract::State,
+    http::Response,
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router, Server,
+};
 use dotenv::dotenv;
 use std::{
     env,
@@ -18,13 +24,14 @@ async fn main() {
 
     let router = Router::new()
         .route("/", get(root_get))
+        .route("/index.js", get(javascript_get))
         .route("/api/cpus", get(cpus_get))
         .with_state(AppState {
             sys: Arc::new(Mutex::new(System::new().into())),
         });
 
     let address: SocketAddr = env::var("SERVER_ADDRESS")
-        .unwrap_or_else(|_| "0.0.0.0:8081".to_string())
+        .unwrap_or_else(|_| "0.0.0.0:8080".to_string())
         .parse()
         .expect("Invalid server address");
 
@@ -39,8 +46,19 @@ async fn main() {
     println!("message: {}", message);
 }
 
-async fn root_get() -> &'static str {
-    "Hello World"
+#[axum::debug_handler]
+async fn root_get() -> impl IntoResponse {
+    let html = tokio::fs::read_to_string("src/index.html").await.unwrap();
+    Html(html)
+}
+
+#[axum::debug_handler]
+async fn javascript_get() -> impl IntoResponse {
+    let js = tokio::fs::read_to_string("src/index.js").await.unwrap();
+    Response::builder()
+        .header("content-type", "application/javascript;charset=utf-8")
+        .body(js)
+        .unwrap()
 }
 
 #[axum::debug_handler]
